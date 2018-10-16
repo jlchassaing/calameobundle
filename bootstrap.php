ff4e377d3a5f2d44659fe50efc74c2a2f4c463bd
Jean-Luc Chassaing <jlchassaing@gmail.com>
2018-10-16 16:29:03 +0200

    add bootstrap

---
+++ bootstrap.php
@@ -0,0 +1,11 @@
+<?php
+
+$autoloadFile = realpath(__DIR__.'/vendor/autoload.php');
+
+
+
+if (!file_exists($autoloadFile)) {
+    throw new RuntimeException('Install dependencies to run test suite.');
+}
+
+require_once $autoloadFile;
