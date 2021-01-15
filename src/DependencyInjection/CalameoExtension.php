85b58b4c30225a1870b985f63e9262cfcee8f31d
Jean-Luc CHASSAING <jlchassaing@gmail.com>
2021-01-15 11:34:24 +0100

    restore services file

--- src/DependencyInjection/CalameoExtension.php
+++ src/DependencyInjection/CalameoExtension.php
@@ -27,7 +27,7 @@ class CalameoExtension extends Extension implements PrependExtensionInterface
     public function load(array $configs, ContainerBuilder $container)
     {
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.yml');
+        $loader->load('services.yaml');
 
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
